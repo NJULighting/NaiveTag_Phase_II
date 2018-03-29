4948f3d156a7f1743189b6c90cbe5a40e6d16255
iznauy <zy05160516@126.com>
2018-03-29 19:38:11 +0800

    init project

---
+++ src/main/java/top/minecode/service/Placeholder.java
@@ -0,0 +1,10 @@
+package top.minecode.service;
+
+/**
+ * Created on 2018/3/29.
+ * Description:
+ *
+ * @author iznauy
+ */
+public class Placeholder {
+}
---
+++ src/main/java/top/minecode/web/Placeholder.java
@@ -0,0 +1,10 @@
+package top.minecode.web;
+
+/**
+ * Created on 2018/3/29.
+ * Description:
+ *
+ * @author iznauy
+ */
+public class Placeholder {
+}
