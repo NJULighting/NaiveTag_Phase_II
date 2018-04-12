81b84a186d0a690367ef9465e5350be8a06c4f06
高梦婷 <161120030@smail.nju.edu.cn>
2018-04-12 19:35:27 +0800

    delete test file

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -3,14 +3,11 @@ import VueRouter from 'vue-router'
 import Home from '../components/worker/workerHomePage.vue'
 import History from '../components/worker/workerHistoryPage.vue'
 import Task from '../components/worker/workerTaskPage.vue'
-import Test from '../components/worker/test.vue'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
 
     routes:[{
-        path: '/Test', component: Test
-    },{
         path: '/Home', component: Home
     },{
         path: '/History', component: History
