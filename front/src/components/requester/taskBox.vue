63f333d1f0311cd1d3b33b3bc16b68bbfa487fff
Jar <924342513@qq.com>
2018-04-14 20:43:40 +0800

    创建任务的部分以及登录注册与后端交互的部分

--- front/src/components/requester/taskBox.vue
+++ front/src/components/requester/taskBox.vue
@@ -1,33 +1,46 @@
 <template>
-    <el-card>
-        <el-container>
-            <el-aside>
-                <img src='/src/assets/logo.png'>
-            </el-aside>
+    <router-link to="taskDetail">
+        <el-card style="margin-top: 20px">
+            <el-container>
+                <el-aside>
+                    <el-progress type="circle" :percentage="task.process*100"></el-progress>
+                </el-aside>
 
-            <el-main>
-                <el-container>
-                    <el-aside>
-                        <div>{{task.taskName}}</div>
-                        <div>任务进度:{{task.progress}}</div>
-                    </el-aside>
-                    <el-main>
-                        <div>3积分/10张</div>
-                    </el-main>
-                </el-container>
+                <el-main style="height: 100%">
 
-            </el-main>
-        </el-container>
-    </el-card>
+                    <el-row>
+                        <el-col :span="3" class="text-col">
+                            <span >{{task.taskName}}</span>
+                        </el-col>
+                        <el-col :span="8" offset="4"  class="text-col">
+                       <span >
+                           截止日期：{{task.endTime}}
+                       </span>
+                        </el-col>
+                        <el-button icon="el-icon-download"  style="margin: 20px" @click.prevent="exportResult">下载结果</el-button>
+                    </el-row>
+
+                </el-main>
+            </el-container>
+        </el-card>
+    </router-link>
 </template>
 
 <script>
     export default {
         name: "taskBox",
-        props: ['task']
+        props: ['task'],
+        methods:{
+            exportResult(){
+
+            }
+        }
     }
 </script>
 
 <style scoped>
+    .text-col{
+       margin-top: 30px;
+    }
 
 </style>
---
+++ front/src/components/requester/taskDetail.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+       任务详情页
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TaskDetail"
+    }
+</script>
+
+<style scoped>
+
+</style>
