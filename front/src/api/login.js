0702d99072133f857afcd7178cd716947e890a2f
高梦婷 <161120030@smail.nju.edu.cn>
2018-04-15 17:12:06 +0800

    update login

--- front/src/api/login.js
+++ front/src/api/login.js
@@ -1,7 +1,30 @@
 export function login(username, password) {
-    axios.post('login.html', {username: username, password: password}).then(function (response) {
-        console.log(response);
-    })
+    // axios.post('login.html', {username: username, password: password}).then(function (response) {
+    //     console.log('login')
+    //     console.log(response);
+    // })
+
+    var xmlhttp;
+    if (window.XMLHttpRequest)
+    {
+        //  IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
+        xmlhttp=new XMLHttpRequest();
+    }
+    else
+    {
+        // IE6, IE5 浏览器执行代码
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function()
+    {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200)
+        {
+          console.log('success');
+          return ;
+        }
+    }
+    xmlhttp.open("POST","http://localhost:8000/naive/login.html?username=1&password=1",true);
+    xmlhttp.send();
 }
 
 function validateFromServer(username, password) {
