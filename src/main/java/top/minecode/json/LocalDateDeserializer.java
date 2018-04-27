6ebc53a02ba366012b2c7e467a934c8add21422b
Jar <924342513@qq.com>
2018-04-27 20:43:18 +0800

    后端解析date

--- src/main/java/top/minecode/json/LocalDateDeserializer.java
+++ src/main/java/top/minecode/json/LocalDateDeserializer.java
@@ -18,10 +18,6 @@ public class LocalDateDeserializer implements JsonDeserializer<LocalDate> {
     public LocalDate deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
 
         String time = jsonElement.getAsString();
-        String[] times = time.split("-");
-        int year = Integer.getInteger(times[0]);
-        int month = Integer.getInteger(times[1]);
-        int day = Integer.getInteger(times[2]);
-        return LocalDate.of(year, month, day);
+        return LocalDate.parse(time);
     }
 }
